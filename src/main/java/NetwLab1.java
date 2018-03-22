f79db5b62bc902bbf1bd34f2548aa7eca9f21eb9
Dmitrii Grishin <dmytriigrishin@gmail.com>
2018-03-22 23:30:05 +0300

    String presentations done(refactor needed)

---
+++ src/main/java/NetwLab1.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+
+public class NetwLab1{
+    public static void main(String[] args) {
+        String input = args[0] + " " + args[1] + " " + args[2];
+        byte[] binaryArray = input.getBytes(Charset.forName("cp1251"));
+        String hexedInput = toHex(binaryArray);
+        System.out.println(hexedInput);
+        String binary;
+        StringBuilder builderForBinary = new StringBuilder();
+        for(byte i: binaryArray) {
+            builderForBinary.append(Integer.toBinaryString(i & 0xFF));
+        }
+        binary = builderForBinary.toString();
+        System.out.println(binary);
+
+
+    }
+    private static String toHex(byte[] binary) {
+        return String.format("%010x", new BigInteger(1, binary));
+    }
+}
