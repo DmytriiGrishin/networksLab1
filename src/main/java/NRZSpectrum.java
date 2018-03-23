1c9aabd3b2160de5641e0a23aa7028dcea4e7481
Dmitrii Grishin <dmytriigrishin@gmail.com>
2018-03-23 22:08:07 +0300

    NRZ freq done

--- src/main/java/NRZSpectrum.java
+++ src/main/java/NRZSpectrum.java
@@ -2,14 +2,12 @@ import java.util.HashMap;
 
 public class NRZSpectrum {
     private HashMap<Integer, Integer> intervals;
-    private Double mean;
-    private Double max;
-    private Double min;
-
+    private int bitsLength;
     NRZSpectrum(String text){
         intervals = new HashMap<Integer, Integer>();
         byte[] bytes = text.getBytes();
         int[] bits = new int[bytes.length];
+        bitsLength = bits.length;
         for(int i = 0; i < bits.length; i++) {
             bits[i] = bytes[i] - 48;
         }
@@ -29,13 +27,28 @@ public class NRZSpectrum {
         intervals.put(counter, intervals.get(counter) == null ? 1 : intervals.get(counter) +1 );
     }
 
-    public Double getMax(){
-        if(max == null){
+    public Double getMax(int c){
+        return (double)(c)/2*7;
+    }
 
-            for(Integer i: intervals.keySet()){
-
+    public Double getMin(int c) {
+        double min;
+        int maxInterval = 0;
+        for (Integer i : intervals.keySet()) {
+            if (i > maxInterval) {
+                maxInterval = i;
             }
         }
-        return max;
+        min = (double) c / (maxInterval * 2);
+        return min;
     }
+
+    public Double getMean(int c) {
+        double mean = 0.0;
+        for (Integer i : intervals.keySet()) {
+            mean += c / 2  * intervals.get(i);
+        }
+        return mean / bitsLength;
+    }
+
 }
--- src/main/java/NetwLab1.java
+++ src/main/java/NetwLab1.java
@@ -6,13 +6,18 @@ public class NetwLab1{
         String input = args[0] + " " + args[1] + " " + args[2];
         byte[] bytes = StringUtils.encode(input, "cp1251");
         String hexedInput = StringUtils.toHex(bytes);
-        System.out.println(hexedInput);
+        System.out.println("Hexed: " + hexedInput + "\nBytes = " + hexedInput.length()/2);
         String binary = StringUtils.toBinary(bytes);
-        System.out.println(binary);
+        System.out.println("Binary: " +binary + "\nBits = " + binary.length());
         NRZGraph nrzGraph = new NRZGraph(800, 200);
         nrzGraph.doMagic(binary);
 
         NRZSpectrum nrzSpectrum = new NRZSpectrum(binary);
+        System.out.println("NRZ:");
+        System.out.println("Max freq in hz: " + nrzSpectrum.getMax(1000000));
+        System.out.println("Min freq in hz: " + nrzSpectrum.getMin(1000000));
+
+        System.out.println("Mean freq in hz: " + nrzSpectrum.getMean(1000000));
     }
 
 }
