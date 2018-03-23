4a8ea988a3407c4036f47aa2956694d88393cb86
DGrishin <DGrishin@croc.ru>
2018-03-23 12:03:57 +0300

    StringUtils refactored

--- src/main/java/NetwLab1.java
+++ src/main/java/NetwLab1.java
@@ -4,21 +4,14 @@ import java.nio.charset.Charset;
 public class NetwLab1{
     public static void main(String[] args) {
         String input = args[0] + " " + args[1] + " " + args[2];
-        byte[] binaryArray = input.getBytes(Charset.forName("cp1251"));
-        String hexedInput = toHex(binaryArray);
+        byte[] bytes = StringUtils.encode(input, "cp1251");
+        String hexedInput = StringUtils.toHex(bytes);
         System.out.println(hexedInput);
-        String binary;
-        StringBuilder builderForBinary = new StringBuilder();
-        for(byte i: binaryArray) {
-            builderForBinary.append(Integer.toBinaryString(i & 0xFF));
-        }
-        binary = builderForBinary.toString();
+        String binary = StringUtils.toBinary(bytes);
         System.out.println(binary);
 
         NRZGraph nrzGraph = new NRZGraph(800, 200);
         nrzGraph.doMagic(binary);
     }
-    private static String toHex(byte[] binary) {
-        return String.format("%010x", new BigInteger(1, binary));
-    }
+
 }
---
+++ src/main/java/StringUtils.java
@@ -0,0 +1,35 @@
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+
+public class StringUtils {
+
+    public static byte[] encode(String text, String charset){
+        return text.getBytes(Charset.forName(charset));
+    }
+
+    public static String toHex(byte[] bytes) {
+        return String.format("%010x", new BigInteger(1, bytes));
+    }
+
+    public static String toHex(String text, String charset) {
+        byte[] bytes = encode(text, charset);
+        return String.format("%010x", new BigInteger(1, bytes));
+    }
+
+    public static String toBinary(byte[] bytes){
+        StringBuilder builder = new StringBuilder();
+        for(byte i: bytes) {
+            builder.append(Integer.toBinaryString(i & 0xFF));
+        }
+        return builder.toString();
+    }
+
+    public static String toBinary(String text, String charset){
+        byte[] bytes = encode(text, charset);
+        StringBuilder builder = new StringBuilder();
+        for(byte i: bytes) {
+            builder.append(Integer.toBinaryString(i & 0xFF));
+        }
+        return builder.toString();
+    }
+}
